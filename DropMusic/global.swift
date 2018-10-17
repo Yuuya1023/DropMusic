0bdee56b60e264983682eec11f3e25e272b6ddf4
Yuuya1023 <wish.on.the.shootingstar@gmail.com>
2018-10-17 23:25:38 +0900

    設定ファイル

---
+++ DropMusic/global.swift
@@ -0,0 +1,19 @@
+//
+//  global.swift
+//  DropMusic
+//
+//  Copyright © 2018年 n.yuuya. All rights reserved.
+//
+
+
+let DROPBOX_APP_KEY = ""
+let TWITTER_CONSUMER_KEY = ""
+let TWITTER_CONSUMER_SECRET_KEY = ""
+
+let NOTIFICATION_DROPBOX_LOGGED_IN  = "DropBoxLoggedIn"
+let NOTIFICATION_DROPBOX_LOGGED_OUT = "DropBoxLoggedOut"
+let NOTIFICATION_DID_CHANGE_AUDIO = "DidChangeAudio"
+let NOTIFICATION_SHOW_AUDIO_PLAYER_VIEW = "ShowAudioPlayerView"
+
+
+let JSON_NAME_PLAYLIST = "playlist.json"
