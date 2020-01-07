d43221ab6b6aaa91fe55e37b8a75cf1d5a2481e5
Yuya Nambu <z.yuya.nambu@G-PC-00397559.intra.gree-office.net>
2020-01-07 17:49:27 +0900

    りふぁくた

--- DropMusic/Manager/AppDataManager.swift
+++ DropMusic/Manager/AppDataManager.swift
@@ -41,8 +41,8 @@ class AppDataManager {
         }
     }
     
-    private let _savePath: String!
     private let _manageDataFilePathPrefix: String!
+    private let _localFilePath: String!
     private let _localTempFilePath: String!
     private var _dropboxUserName: String = ""
     var dropboxUserName: String? {
@@ -86,11 +86,11 @@ class AppDataManager {
         if let username = UserDefaults.standard.string(forKey: USER_DEFAULT_DROPBOX_USERNAME) {
             _dropboxUserName = username
         }
-        _savePath = DownloadFileManager.sharedManager.getCachePath(storageType: .DropBox, add: "") + "/" + JSON_NAME
         _manageDataFilePathPrefix = "/DropMusic/"
-        _localTempFilePath = DownloadFileManager.sharedManager.getCachePath(storageType: .DropBox, add: "") + "/temp_" + JSON_NAME
+        _localFilePath = DownloadFileManager.sharedManager.getCachePath(storageType: .DropBox, add: "") + "/" + JSON_NAME
+        _localTempFilePath = _localFilePath+".tmp"
         // 通信環境などにより読み込めなかった時のためにローカルファイルを読み込んでおく.
-        if let data = loadData(path: _savePath) {
+        if let data = loadData(path: _localFilePath) {
             _manageData = data
         }
     }
@@ -139,7 +139,7 @@ class AppDataManager {
         let encoder = JSONEncoder()
         do {
             let data = try encoder.encode(_manageData)
-            try data.write(to: URL(fileURLWithPath: _savePath))
+            try data.write(to: URL(fileURLWithPath: _localFilePath))
         } catch {
             print("json convert failed in JSONEncoder", error.localizedDescription)
         }
@@ -156,7 +156,7 @@ class AppDataManager {
             return
         }
         _isLoading = true
-        let localFile = loadData(path: _savePath)
+        let localFile = loadData(path: _localFilePath)
         
         // 保存先.
         let destination : (URL, HTTPURLResponse) -> URL = { temporaryURL, response in
@@ -254,8 +254,8 @@ class AppDataManager {
         _dropboxUserName = ""
         UserDefaults.standard.removeObject(forKey: USER_DEFAULT_DROPBOX_USERNAME)
         // ファイル削除.
-        try! FileManager.default.removeItem(at: URL(fileURLWithPath: self._savePath))
-        try! FileManager.default.removeItem(at: URL(fileURLWithPath: self._localTempFilePath))
+        try! FileManager.default.removeItem(at: URL(fileURLWithPath: _localFilePath))
+        try! FileManager.default.removeItem(at: URL(fileURLWithPath: _localTempFilePath))
     }
     
 }
